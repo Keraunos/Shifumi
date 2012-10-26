7a6e05e8a69ac4198869a11b30ba2b4032e9e75f
Keraunos <gaetan.girin.eu@gmail.com>
2012-10-26 14:10:45 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: gaetan
+ *
+ * Created on 26 octobre 2012, 14:05
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
